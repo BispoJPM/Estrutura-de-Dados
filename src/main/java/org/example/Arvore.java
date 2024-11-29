4c4947517c50c1319bff4bc450a994784631ca63
João Pedro <142633184+BispoJPM@users.noreply.github.com>
2024-11-28 22:46:12 -0300

    Sakaue

--- src/main/java/org/example/Arvore.java
+++ src/main/java/org/example/Arvore.java
@@ -23,24 +23,81 @@ class Arvore {
         return raiz;
     }
     
-    Arvore buscaRecursiva(Arvore raiz, int valor) {
-        if (raiz == null || raiz.conteudo == valor) {
-            return raiz;
-        }
-        if (raiz.conteudo > valor) {
-            return buscaRecursiva(raiz.esq, valor);
-        } else {
-            return buscaRecursiva(raiz.dir, valor);
-        }
-    }
+    // Arvore buscaRecursiva(Arvore raiz, int valor) {
+    //     if (raiz == null || raiz.conteudo == valor) {
+    //         return raiz;
+    //     }
+    //     if (raiz.conteudo > valor) {
+    //         return buscaRecursiva(raiz.esq, valor);
+    //     } else {
+    //         return buscaRecursiva(raiz.dir, valor);
+    //     }
+    // }
 
-    Arvore buscaIterativa(Arvore raiz, int valor) {
-        while (raiz != null && raiz.conteudo != valor) {
-            if (raiz.conteudo >valor) {
-                raiz = raiz.esq;
-            } else {
-                raiz = raiz.dir;
+    // Arvore buscaIterativa(Arvore raiz, int valor) {
+    //     while (raiz != null && raiz.conteudo != valor) {
+    //         if (raiz.conteudo >valor) {
+    //             raiz = raiz.esq;
+    //         } else {
+    //             raiz = raiz.dir;
+    //         }
+    //     }
+    //     return raiz;
+    // }
+
+        void imprimeInOrderLinha(Arvore raiz) {
+            if (raiz != null) {
+                imprimeInOrderLinha(raiz.esq); 
+                System.out.println(raiz.conteudo);
+                imprimeInOrderLinha(raiz.dir);
             }
+        }
+
+    Arvore remove(Arvore raiz, int valor) {
+        if (raiz == null) {
+            return null;
+        }
+
+        if (valor < raiz.conteudo) {
+            raiz.esq = remove(raiz.esq, valor);
+        } else if (valor > raiz.conteudo) {
+            raiz.dir = remove(raiz.dir, valor);
+        } else {
+            if (raiz.esq == null && raiz.dir == null) {
+                return null;
+            }
+            else if (raiz.esq == null) {
+                return raiz.dir;
+            } else if (raiz.dir == null) {
+                return raiz.esq;
+            }
+            else {
+                Arvore maiorzinho = encontraMaior(raiz.esq);
+                raiz.conteudo = maiorzinho.conteudo;
+                raiz.esq = remove(raiz.esq, maiorzinho.conteudo);
+            }
+        }
+        return raiz;
+    }
+            // else {
+            //             Arvore minimuzao = encontraMinimo(raiz.dir);
+            //             raiz.conteudo = minimuzao.conteudo;
+            //             raiz.dir = remove(raiz.dir, minimuzao.conteudo);
+            //         }
+            //     }
+            //     return raiz;
+            // }
+
+    // Arvore encontraMinimo(Arvore raiz) {
+    //     while (raiz.esq != null) {
+    //         raiz = raiz.esq;
+    //     }
+    //     return raiz;
+    // }
+
+    Arvore encontraMaior(Arvore raiz) {
+        while (raiz.dir != null) {
+            raiz = raiz.dir;
         }
         return raiz;
     }
@@ -55,19 +112,22 @@ class Arvore {
         raiz = arvore.insere(raiz, 7);
         raiz = arvore.insere(raiz, 12);
         raiz = arvore.insere(raiz, 18);
+        // raiz = arvore.remove(raiz, 15);  
+        System.out.println("Valores da Árvore (um embaixo do outro):");
+            arvore.imprimeInOrderLinha(raiz);
+        
+        // Arvore resultadoRecursivo = arvore.buscaRecursiva(raiz, 7);
+        // if (resultadoRecursivo != null) {
+        //     System.out.println("Recursivamente: " + resultadoRecursivo.conteudo);
+        // } else {
+        //     System.out.println("Não tem.");
+        // }
 
-        Arvore resultadoRecursivo = arvore.buscaRecursiva(raiz, 7);
-        if (resultadoRecursivo != null) {
-            System.out.println("Recursivamente: " + resultadoRecursivo.conteudo);
-        } else {
-            System.out.println("Não tem.");
-        }
-
-        Arvore resultadoIterativo = arvore.buscaIterativa(raiz, 12);
-        if (resultadoIterativo != null) {
-            System.out.println("Iterativamente: " + resultadoIterativo.conteudo);
-        } else {
-            System.out.println("Não tem.");
+        // Arvore resultadoIterativo = arvore.buscaIterativa(raiz, 12);
+        // if (resultadoIterativo != null) {
+        //     System.out.println("Iterativamente: " + resultadoIterativo.conteudo);
+        // } else {
+        //     System.out.println("Não tem.");
         }
     }
 }
